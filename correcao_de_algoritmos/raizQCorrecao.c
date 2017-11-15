46b7bc86c111fdc1fa47a7d37b82bb0d9e2a00f1
Ricardo Petronilho <rpetronilho98@gmail.com>
2017-11-15 22:11:41 +0000

    adicionei um algoritmo para calcular a raiz quadrada inteira de um numero inteiro, e as condições de validade do programa, através da função assert

---
+++ correcao_de_algoritmos/raizQCorrecao.c
@@ -0,0 +1,42 @@
+#include <assert.h>
+#include <stdio.h>
+
+int raizQ(int n);
+
+
+int main(){
+
+	int n = 144;
+
+	printf("\nraizQ de %d = %d\n\n", n, raizQ(n));
+	
+	return 0;
+}
+
+
+
+int raizQ(int n){
+
+	int n0 = n; // variável lógica apenas para guardar o valor inicial de n
+
+	assert(n == n0 && n > 0); // pré-condição do programa
+	
+	int r = 1;
+
+	assert(n == n0 && n >= 0 && r == 1); // pré-condição do ciclo While
+
+	while (r * r < n){
+
+		assert(n == n0 && n >= 0 && (r - 1) * (r - 1) <= n); // Invariante
+		
+		r = r + 1;
+	}
+
+	assert(n == n0 && n >= 0 && (r - 1) * (r - 1) <= n && r * r >= n); // pós-condição do ciclo While
+	
+	if (r * r > n) r = r - 1;
+	
+	assert(r*r <= n0 && (r+1)*(r+1) > n0); // pós-condição do programa
+
+	return r;
+}
